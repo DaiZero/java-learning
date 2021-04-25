465ba89aa7261452384b864fa5bf75937ff55824
DZero <dzd624175217@gmail.com>
2021-04-25 11:24:51 +0800

    Excel填充并且合并单元格

--- demo-excel/src/main/java/easyexcel/FillTest001.java
+++ demo-excel/src/main/java/easyexcel/FillTest001.java
@@ -2,20 +2,33 @@ package easyexcel;
 
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.metadata.Head;
+import com.alibaba.excel.write.merge.AbstractMergeStrategy;
 import com.alibaba.excel.write.metadata.WriteSheet;
+import com.sun.javafx.collections.MappingChange;
 import easyexcel.dao.ProjectPayment;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.util.CellRangeAddress;
 
 import java.io.File;
+import java.lang.reflect.Field;
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+
+
+import java.util.stream.Collectors;
 
 /**
  * @author dzero
  */
 public class FillTest001 {
-    private static void export001() {
+
+    public static void main(String[] args) throws Exception {
+        export001();
+    }
+
+    private static void export001() throws Exception {
         List list = new ArrayList<ProjectPayment>();
         for (int i = 0; i < 5; i++) {
             String pm = "项目经理00" + i;
@@ -33,7 +46,20 @@ public class FillTest001 {
         list.forEach(System.out::println);
         String templateFileName = ClassLoader.getSystemResource("template/FillTest001.xlsx").getPath();
         String fileName = "D:/temp/" + "listFill" + System.currentTimeMillis() + ".xlsx";
-        EasyExcel.write(fileName).withTemplate(templateFileName).sheet().doFill(list);
+        Object collect = list.stream().collect(Collectors.groupingBy(ProjectPayment::getProjectManager, Collectors.counting()));
+        HashMap<String, Long> hashMap;
+        if (collect instanceof HashMap) {
+            hashMap = (HashMap<String, Long>) collect;
+        } else {
+            return;
+        }
+        List<Integer> gl = new ArrayList<>();
+        hashMap.forEach((k, v) -> {
+            gl.add(Integer.valueOf(v.toString()));
+        });
+        System.out.println(collect);
+        MyMergeStrategy myMergeStrategy = new MyMergeStrategy(list, gl);
+        EasyExcel.write(fileName).withTemplate(templateFileName).registerWriteHandler(myMergeStrategy).sheet().doFill(list);
     }
 
     public static int randInt(int min, int max) {
@@ -47,8 +73,76 @@ public class FillTest001 {
         return randomNum;
     }
 
-    public static void main(String[] args) {
-        export001();
+
+    public static Map<String, Object> objectToMap(Object obj) throws Exception {
+        if (obj == null) {
+            return null;
+        }
+
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        Field[] declaredFields = obj.getClass().getDeclaredFields();
+        for (Field field : declaredFields) {
+            field.setAccessible(true);
+            map.put(field.getName(), field.get(obj));
+        }
+
+        return map;
     }
 
+
+    static class MyMergeStrategy extends AbstractMergeStrategy {
+
+        private List<Object> fruitList;
+        private List<Integer> fruitGroupCount;
+        private Sheet sheet;
+
+        public MyMergeStrategy(List<Object> fruitList, List<Integer> fruitGroupCount) {
+            this.fruitList = fruitList;
+            this.fruitGroupCount = fruitGroupCount;
+        }
+
+        // 将该列全部合并成一个单元格
+        private void mergeCommonColumn(Integer index) {
+            CellRangeAddress cellRangeAddress = new CellRangeAddress(1, fruitList.size(), index, index);
+            sheet.addMergedRegionUnsafe(cellRangeAddress);
+        }
+
+        // 按照分组将各种类别分别合并成一个单元格
+        private void mergeGroupColumn(Integer index) {
+            Integer rowCnt = 1;
+            for (Integer count : fruitGroupCount) {
+                int lastCnt=rowCnt + count - 1;
+                if(lastCnt>rowCnt){
+                    CellRangeAddress cellRangeAddress = new CellRangeAddress(rowCnt, rowCnt + count - 1, index, index);
+                    sheet.addMergedRegionUnsafe(cellRangeAddress);
+                }
+                rowCnt += count;
+            }
+        }
+
+        @Override
+        protected void merge(Sheet sheet, Cell cell, Head head, Integer integer) {
+            this.sheet = sheet;
+            if (cell.getRowIndex() == 1) {
+                switch (cell.getColumnIndex()) {
+                    case 0:
+                        this.mergeGroupColumn(0);
+                        break;
+//                    case 1:
+//                        this.mergeGroupColumn(1);
+//                        break;
+//                    case 2:
+//                        break;
+//                    case 3:
+//                        this.mergeCommonColumn(3);
+//                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
 }
+
+
